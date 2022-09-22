adfb5cb6e2bb92247a3f7dd9a53e260b95bf639d
Dave Longman <dlongman@users.noreply.github.com>
2022-09-22 12:34:03 +0100

    added docstring comments

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Inspired from https://www.pythongasm.com/menubar-app-for-macos-using-python/ article
 # Icons from https://www.flaticon.com/free-icons/diabetes
-
+"""Simple macOS menubar application that displays current blood glucose
+information from a Nightscout installation
+"""
 import threading
 import logging
 import os
@@ -11,6 +13,10 @@ import rumps
 from nightscout import Nightscout
 
 class NightscoutMenuApp(rumps.App):
+    """Creates a macOS menubar app that displays blood glucose data from Nightscout
+
+    Uses the rumps package to create the macOS UI
+    """
 
     nightscoutUrl = "https://d9n-nightscout.herokuapp.com"
 
@@ -24,20 +30,27 @@ class NightscoutMenuApp(rumps.App):
 
     @rumps.clicked("Launch Nightscout")
     def launch_website(self, sender):
+        """Launches the Nightscout website in a browser window"""
         logging.info("Launching Nightscout website")
         webbrowser.open(self.nightscoutUrl, new=0)
 
     @rumps.timer(5)
     def refresh_data(self, sender):
+        """Refreshes the Nightscout data every 5 seconds in a separate thread"""
         thread = threading.Thread(target=self.get_bg)
         thread.start()
 
     def get_bg(self):
+        """Refreshes the Nightscout data and displays the data in the menubar"""
         self.nightscout.refresh()
         self.title = self.nightscout.text
 
 def configure_logging():
-
+    """Configures file and console logging, file logging logs to a file
+    location defined in the environment variable NS_LOG_FILE and logs
+    INFO output or a logging level defined in environment variable NS_LOG_LEVEL.
+    The console logger logs DEBUG level messages
+    """
     print(os.getenv('NS_LOG_FILE'))
     log_file = os.getenv('NS_LOG_FILE') or \
         "/Users/Dave/nightscout/menubar-app/nightscout_menubar.log"
--- nightscout.py
+++ nightscout.py
@@ -1,3 +1,4 @@
+"""Retrieves the latest entry from a Nightscout installation"""
 import logging
 import json
 
@@ -7,21 +8,52 @@ import requests
 
 
 class Nightscout():
+    """Simple wrapper around the Nightscout entries api. Retrieves the latest
+    blood glucose reading when refresh() is called and exposes the data value
+    in mmol/L or mg/dL depending on the display_mmol setting with the value
+    property. A string representation of the current value and trend is
+    exposed in the title property.
 
+    Attributes:
+    -----------
+        value: float
+            The current blood glucose reading
+        title: str
+            The current blood glucose reading and trend indicator
+        display_mmol: bool
+            If True the mmol/L value is used for the current blood glucose
+            value, otherwise the mg/dL value is used.
+        low_warning_level: int
+            The value in mg/dL that indicates a low blood glucose reading
+        high_warning_level: int
+            The value in mg/dL that indicates a high blood glucose reading
+        is_low: bool
+            True is the current blood glucose value is below the low_warning_level
+        in_range: bool
+            True if the current blood glucose value is within the low_ and
+            high_ warning levels, else False
+        is_stale: bool
+            True if the last reading was longer ago than the stale_reading_period
+        stale_reading_period: int
+            The number of minutes that should elapse before a reading is considered stale
+        data_refresh_period: int
+            The number of seconds that should elapse before a new api request is made
+        last_refreshed: datetime
+            The time the data was last retrieved from the api
+
+    Methods:
+    --------
+    refresh():
+        Gets the latest data from the entries api. Only refreshes the data once
+        within the data_refresh_period to reduce api call volume.
+    """
+    # public attributes
     low_warning_level = 80 # 80 mg/dL == 4.5 mmol/L
-    stale_reading_period = -15
-    direction_indicator = {
-        "DoubleUp": "⇈",
-        "SingleUp": "↑",
-        "FortyFiveUp": "↗",
-        "Flat": "→",
-        "FortyFiveDown": "↘",
-        "SingleDown": "↓",
-        "DoubleDown": "⇊",
-        "NONE": "⇼"
-    }
+    high_warning_level = 240 # 240 mg/dL == 13.3 mmol/L
     title = ""
     display_mmol = True
+    is_low = False
+    stale_reading_period = -15
     last_refreshed = datetime.min
     data_refresh_period = 60
 
@@ -32,7 +64,10 @@ class Nightscout():
         self.refresh()
 
     def refresh(self):
-
+        """Retrieves the latest blood glucose reading from the Nightscout
+        API, calculates the mmol/L equivalent of the mg/dL reading and
+        generates a simple display string with the current value and trend
+        """
         # don't bother refreshing if the data was refreshed more
         # recently than the standard sgv reporting frequency
         if not self.data_needs_refresh():
@@ -50,6 +85,7 @@ class Nightscout():
         logging.debug("Api returned data %s", json.dumps(ns_data))
 
         self.mgdl = ns_data["sgv"]
+
         calculated_mmol = float(self.mgdl)/18
         self.mmol = f"{calculated_mmol:.1f}"
         if self.display_mmol:
@@ -57,11 +93,12 @@ class Nightscout():
         else:
             self.value = self.mgdl
 
-        self.direction = self.direction_indicator[ns_data["direction"]] or "↛"
+        self.reading_in_range(self.mgdl)
+
+        self.direction = self.get_direction_indicator(ns_data["direction"])
         self.date = datetime.strptime(ns_data["dateString"], "%Y-%m-%dT%H:%M:%S.%fZ")
         self.is_stale = self.date > datetime.now() - timedelta(minutes=self.stale_reading_period)
 
-        # TODO highlight out of range readings
         self.text = f"{self.value}{self.direction}"
 
         # record when data was last refreshed so we can reduce the number of api requests
@@ -69,7 +106,36 @@ class Nightscout():
 
         logging.debug("OBJECT: %s", str(self))
 
+    def reading_in_range(self, value: int):
+        """Compares current reading to high and low thresholds"""
+        if value >= self.high_warning_level:
+            self.in_range = False
+            self.is_low = False
+        elif value <= self.low_warning_level:
+            self.in_range = False
+            self.is_low = True
+        else:
+            self.in_range = True
+            self.is_low = False
+
+    def get_direction_indicator(self, direction: str) -> str:
+        """Return a more friendly direction indicator based on the api return value"""
+        direction_indicator = {
+            "DoubleUp": "⇈",
+            "SingleUp": "↑",
+            "FortyFiveUp": "↗",
+            "Flat": "→",
+            "FortyFiveDown": "↘",
+            "SingleDown": "↓",
+            "DoubleDown": "⇊",
+            "NONE": "⇼"
+        }
+        return direction_indicator[direction] or "↛"
+
     def data_needs_refresh(self) -> bool:
+        """Determines whether the time since the last api call was long
+        enough to warrant a new api call. By default this is 60s as this
+        is the minimum gap between readings in most CGM's"""
         seconds_since_last_refresh = (datetime.now() - self.last_refreshed).total_seconds()
         return seconds_since_last_refresh >= self.data_refresh_period
 
