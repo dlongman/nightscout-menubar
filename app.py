84f7bcf1fc9dfe9a4d17db6d1fe5135594ef7593
Dave Longman <dlongman@users.noreply.github.com>
2022-09-22 09:00:19 +0100

    Added logging

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 import threading
 import rumps
 import webbrowser
+import logging
+import os
 
 from nightscout import Nightscout
 
@@ -13,11 +15,15 @@ class NightscoutMenuApp(rumps.App):
 
     def __init__(self):
         super(NightscoutMenuApp, self).__init__(name="NightscoutMenuApp")
+        logging.info("Initialising NightscoutMenuApp class")
+        logging.info(f"Nightscout base url is {self.nightscoutUrl}")
+
         self.icon = "icon.png"
         self.ns = Nightscout(self.nightscoutUrl)
 
     @rumps.clicked("Launch Nightscout")
     def launchNightscout(self, sender):
+        logging.info("Launching Nightscout website")
         webbrowser.open(self.nightscoutUrl, new=0)
 
     @rumps.timer(5)
@@ -28,6 +34,36 @@ class NightscoutMenuApp(rumps.App):
     def getBloodGlucose(self):
         self.ns.refresh()
         self.title = self.ns.text
-       
+
+def configure_logging():
+	
+    print(os.getenv('NS_LOG_FILE'))
+    log_file = os.getenv('NS_LOG_FILE') or "/Users/Dave/nightscout/menubar-app/nightscout_menubar.log"
+    log_level = os.getenv('NS_LOG_LEVEL') or logging.INFO
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # create file handler for file based logging
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(log_level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # create console handler for INFO messages
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
 if __name__ == '__main__':
-    NightscoutMenuApp().run()
+    try:
+        configure_logging()
+        logging.info("Logging configured, launching application")
+        NightscoutMenuApp().run()
+        logging.info("Exiting")
+    except Exception as e:
+        logging.error(e)
+    finally:
+        logging.info("Exiting application")
--- nightscout.py
+++ nightscout.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from datetime import timedelta
 import requests
+import logging
+import json
 
 class Nightscout():
     
@@ -18,20 +20,32 @@ class Nightscout():
     }
     title = ""
     display_mmol = True
+    last_refreshed = datetime.min
+    data_refresh_period = 60
     
     def __init__(self, base_url: str):
+        logging.info("Initialising Nighscout class")
         self.reading_is_stale = None
         self.url = f"{base_url}/api/v1/entries.json?count=1"
         self.refresh()
 
     def refresh(self):
         
+        # don't bother refreshing if the data was refreshed more 
+        # recently than the standard sgv reporting frequency
+        if not self.data_needs_refresh():
+            logging.info("Skipping data refresh")
+            return
+
+        logging.info(f"Getting entries from nightscout api using {self.url}")
         response = requests.get(self.url)
         if response.status_code!=200:
+            logging.debug(f"Api returned status code {response.status_code}, exiting")
             self.text = "API Error"
             return
 
         ns_data = response.json()[0]
+        logging.debug(f"Api returned data {json.dumps(ns_data)}")
 
         self.mgdl = ns_data["sgv"]
         self.mmol = "{:.1f}".format(float(self.mgdl)/18)
@@ -47,5 +61,15 @@ class Nightscout():
         # TODO highlight out of range readings
         self.text = f"{self.value}{self.direction}"
 
-    
+        # record when data was last refreshed so we can reduce the number of api requests
+        self.last_refreshed = datetime.now()
+
+        logging.debug(f"{self}")
+
+    def data_needs_refresh(self) -> bool:
+        seconds_since_last_refresh = (datetime.now() - self.last_refreshed).total_seconds()
+        return seconds_since_last_refresh >= self.data_refresh_period
+
+    def __str__(self):
+        return f"Latest reading from {self.date}\n\ttext={self.text}\n\tis_stale={self.is_stale}\n\tmmol={self.mmol}\n\tmgdl={self.mgdl}"
     
